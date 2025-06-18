fc8abf907c9567637e07a94adc7d05f803c76dad
samuelcordero <samuelcorderorodrigo@gmail.com>
2025-06-18 12:46:12 +0200

    initial project structure

---
+++ srcs/main.cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+
+int main(int argc, char **argv) {
+	//parser
+	//create tcpsocket-poll
+	//init gameLogic
+	/*while !exit {
+		TCPinputOutput
+		GameLogic
+	}*/
+	return 0;
+}
