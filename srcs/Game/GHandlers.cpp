1bc284a4037d5c8903d1f2645873e53f8429a172
samuelcordero <samuelcorderorodrigo@gmail.com>
2025-07-31 22:22:34 +0200

    broadcast ready

--- srcs/Game/GHandlers.cpp
+++ srcs/Game/GHandlers.cpp
@@ -225,13 +225,13 @@ std::pair<int, int>	Game::dda(Player *origin, Player *dest, int y_mod, int x_mod
 	double x = ax;
 	double y = ay;
 
-	for (int i = 0; i < step; i++)
+	for (int i = 0; i < step - 1; i++)
 	{
 		std::cout << "(" << y << ", " << x << ")" << std::endl;
 		y += inc_y;
 		x += inc_x;
 	}
-	std::cout << "DEFORE CONVERSION: (" << y << ", " << x << ")" << std::endl;
+	std::cout << "BEFORE CONVERSION: (" << y << ", " << x << ")" << std::endl;
 	y -= (y_mod * this->map_height);
 	x -= (x_mod * this->map_width);
 	std::cout << "AFTER CONVERSION: (" << y << ", " << x << ")" << std::endl;
@@ -258,8 +258,74 @@ uint8_t Game::get_sound_direction(Player *origin, Player *dest)
 		}
 	}
 	std::cout << "Nearest enemie is in map: (" << closest_y << ", " << closest_x << ")" << std::endl;
-	dda(origin, dest, closest_y, closest_x);
-	return ('1');
+	std::pair<int,int> coords = dda(origin, dest, closest_y, closest_x);
+
+	int cnt = 1;
+	int dy, dx, py, px, i;
+
+	switch (dest->get_dir()) {
+		case 'N':
+			dy = 0;
+			dx = -1;
+			py = dest->get_y() - 1;
+			px = dest->get_x();
+			break;
+		case 'S':
+			dy = 0;
+			dx = 1;
+			py = dest->get_y() + 1;
+			px = dest->get_x();
+			break;
+		case 'W':
+			dy = 1;
+			dx = 0;
+			py = dest->get_y();
+			px = dest->get_x() - 1;
+			break;
+		case 'E':
+			dy = -1;
+			dx = 0;
+			py = dest->get_y();
+			px = dest->get_x() + 1;
+			break;
+		default:
+			break;
+	}
+
+	std::cout << "Checking player at (" << std::to_string(dest->get_y()) << ", " << std::to_string(dest->get_x()) << ") looking at " << dest->get_dir() + 'A' << std::endl;
+
+	for (i = 1; i < 9; ++i) {
+		if (py < 0) py = map_height - 1;
+		if (px < 0) px = map_width - 1;
+		if (py == map_height) py = 0;
+		if (px == map_width) px = 0;
+		std::cout << "Checking coord (" << std::to_string(px) << ", " << std::to_string(py) << "), looking for (" << std::to_string(coords.second) << ", " << std::to_string(coords.first) << ")\n";
+		if (py == coords.first && px == coords.second) {
+			std::cout << "Found!!!\n";
+			break ;
+		}
+		py += dy;
+		px += dx;
+		++cnt;
+		if (cnt == 2) {
+			if (dy == -1 && dx == 0) {
+				dy = 0;
+				dx = -1;
+			} else if (dy == 1 && dx == 0) {
+				dy = 0;
+				dx = 1;
+			} else if (dy == 0 && dx == -1) {
+				dy = 1;
+				dx = 0;
+			} else if (dy == 0 && dx == 1) {
+				dy = -1;
+				dx = 0;
+			}
+			cnt = 0;
+		}
+	}
+
+	return (i);
 }
 
 void Game::_Broadcast(Player *p)
@@ -270,8 +336,7 @@ void Game::_Broadcast(Player *p)
 	{
 		if (player->get_handshake() == true && player != p)
 		{
-			get_sound_direction(p, player);
-			player->set_send_buffer("message 1," + msg + "\n");
+			player->set_send_buffer("message " + std::to_string(get_sound_direction(p, player)) + "," + msg + "\n");
 		}
 	}
 	(void)p;
