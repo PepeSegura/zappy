b99768d92e9402247d56f8a068358283bd68f46c
Pepe Segura <psegura-@student.42madrid.com>
2025-08-17 13:05:36 +0200

    clean

--- srcs/Game/Handlers/Fork.cpp
+++ srcs/Game/Handlers/Fork.cpp
@@ -13,7 +13,7 @@ void Game::_Fork(Player *p)
 
 	egg->set_x(p->get_x());
 	egg->set_y(p->get_y());
-	
+
 	this->teams[p->get_team_name()].inc_max_conns();
 	this->teams[p->get_team_name()].add_player(egg);
 	this->map[p->get_y()][p->get_x()].add_player_to_team(egg);
